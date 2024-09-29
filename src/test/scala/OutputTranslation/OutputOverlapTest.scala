bd5d5cbdeb186740fbf914554a5a42b33602e9fb
Weiqifan1 <cmlykke@hotmail.com>
2024-09-29 11:26:24 +0200

    updated OutputOverlapTests

--- src/test/scala/OutputTranslation/OutputOverlapTest.scala
+++ src/test/scala/OutputTranslation/OutputOverlapTest.scala
@@ -35,24 +35,24 @@ class OutputOverlapTest extends AnyFlatSpec with Matchers {
   // 4 codes - or less
 
   val fourCodeOrLessJunda: SortedMap[String, List[OutputEntry]] = jundaAboveNine.filter(x => x._1.size <= 4)
-  val fourCodeCodeJundaPair: List[(String, Int)] =
+  val fourCodeCodeJundaPair: List[(String, Int, String)] =
     fourCodeOrLessJunda.flatMap { case (key, entries) =>
       for {
         firstEntry <- entries.headOption
         firstGrapheme <- firstEntry.jundaReverseOrder.headOption
         jundaData <- firstGrapheme.junda
-      } yield (key, jundaData.ordinal)
-    }.toList.sortBy { case (key, ordinal) => (ordinal, key) }
+      } yield (key, jundaData.ordinal, firstGrapheme.char)
+    }.toList.sortBy { case (key, ordinal, chinese) => (ordinal, key, chinese) }
 
   val fourCodeOrLessTzai: SortedMap[String, List[OutputEntry]] = tzaiAboveNine.filter(x => x._1.size <= 4)
-  val fourCodeCodeTzaiPair: List[(String, Int)] =
+  val fourCodeCodeTzaiPair: List[(String, Int, String)] =
     fourCodeOrLessTzai.flatMap { case (key, entries) =>
       for {
         firstEntry <- entries.headOption
         firstGrapheme <- firstEntry.tzaiReverseOrder.headOption
         tzaiData <- firstGrapheme.tzai
-      } yield (key, tzaiData.ordinal)
-    }.toList.sortBy { case (key, ordinal) => (ordinal, key) }
+      } yield (key, tzaiData.ordinal, firstGrapheme.char)
+    }.toList.sortBy { case (key, ordinal, chineseStr) => (ordinal, key, chineseStr) }
 
   // five codes
 
@@ -112,6 +112,32 @@ class OutputOverlapTest extends AnyFlatSpec with Matchers {
     val tzaiTest = fiveTzaiPair
     jundaTest(0)._2 shouldBe 1168
     tzaiTest(0)._2 shouldBe 621
+    
+    val jundaStr = tuppleToStr(jundaTest)
+    val tzaiStr = tuppleToStr(tzaiTest)
+    
+    jundaStr shouldBe """(fgozz, 1168, 丁夫)
+                        |(fongt, 1169, 赵县)
+                        |(ngwdt, 1222, 冒头)
+                        |(fongo, 1272, 茶具)
+                        |(wowdt, 1721, 浅海)
+                        |(fongg, 1725, 茶晶)
+                        |(ngngg, 1725, 晶晶)
+                        |(fgngt, 1838, 霸县)
+                        |(kojgz, 2187, 猴子)
+                        |(fhffo, 2242, 坑蒙)""".stripMargin
+    
+    tzaiStr shouldBe """(fonfy, 621, 救國)
+                       |(wonfo, 863, 寶典)
+                       |(fongo, 946, 載具)
+                       |(dongo, 1032, 成縣)
+                       |(fodng, 1134, 頂面)
+                       |(fofxo, 1160, 歡聚)
+                       |(fhffo, 1167, 西藥)
+                       |(fofho, 1193, 東歐)
+                       |(foozz, 1198, 賢人)
+                       |(flngo, 1246, 零數)""".stripMargin
+    
     val test = ""
   }
 
@@ -119,10 +145,58 @@ class OutputOverlapTest extends AnyFlatSpec with Matchers {
     val jundaTest = fourCodeCodeJundaPair
     val tzaiTest = fourCodeCodeTzaiPair
     jundaTest(0)._2 shouldBe 5105
-    val junda2 = OutputSorting.mapFullJunda.get("ngfo").get
-    val junda3 = OutputSorting.mapFullJunda.get("agnj").get
     tzaiTest(0)._2 shouldBe 5412
+
+    val jundaStr = tuppleToStr(jundaTest)
+    val tzaiStr = tuppleToStr(tzaiTest)
+
+    jundaStr shouldBe """(ngfo, 5105, 題)
+                        |(fxgo, 5209, 聩)
+                        |(dngo, 5772, 砹)
+                        |(efoo, 5898, 糇)
+                        |(yglo, 5941, 锿)
+                        |(ygkl, 6247, 锾)
+                        |(pnfg, 6310, 鲭)
+                        |(pnfo, 6451, 鳜)
+                        |(ygho, 6536, 铗)
+                        |(ffdo, 6578, 趱)""".stripMargin
+
+    tzaiStr shouldBe """(fxno, 5412, 鞅)
+                       |(dngo, 5477, 饜)
+                       |(nfho, 5724, 嘳)
+                       |(ngfo, 5811, 暪)
+                       |(wfho, 5821, 漯)
+                       |(wfhg, 6234, 澠)
+                       |(fffg, 6434, 莤)
+                       |(pnfo, 6470, 鯕)
+                       |(kfho, 6856, 嬠)
+                       |(ffno, 6869, 藈)""".stripMargin
     val test = ""
+  }
+
+
+  private def tuppleTwoToStr(input: List[(String, Int)]): String = {
+    if (input.size > 9) {
+      val res  = input.take(10).map(x =>  "(" + x._1 +", " + x._2 + ")").mkString("\r\n").trim
+      return res
+    } else if (input.size > 0) {
+      val res = input.map(x => "(" + x._1 + ", " + x._2 + ")").mkString("\r\n").trim
+      return res
+    } else {
+      return "None"
+    }
+  }
+  
+  private def tuppleToStr(input: List[(String, Int, String)]): String = {
+    if (input.size > 9) {
+      val res  = input.take(10).map(x =>  "(" + x._1 +", " + x._2 + ", " + x._3 + ")").mkString("\r\n").trim
+      return res
+    } else if (input.size > 0) {
+      val res = input.map(x => "(" + x._1 + ", " + x._2 + ", " + x._3 + ")").mkString("\r\n").trim
+      return res
+    } else {
+      return "None"
+    }
   }
 
 
