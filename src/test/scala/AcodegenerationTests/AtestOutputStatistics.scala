fb19412cf34603410b8b1852410480ae7bc27d90
Weiqifan1 <cmlykke@hotmail.com>
2025-11-14 14:41:50 +0100

    created list of all characters among the 6000 most common that are beyond the first 9

--- src/test/scala/AcodegenerationTests/AtestOutputStatistics.scala
+++ src/test/scala/AcodegenerationTests/AtestOutputStatistics.scala
@@ -133,11 +133,84 @@ class AtestOutputStatistics extends AnyFlatSpec with Matchers {
     failedOrClose shouldBe Set(3572)
 
     val getFailed = AsingletonsForTests.tzai.filter(x => failedOrClose.contains(x._2)).map(x => x._1).toSet
-    getFailed shouldBe Set("葔") // "葔" is common according to Tzai, but is not in Cedict dictionary
-
+    getFailed shouldBe Set("葔")
+    // "葔" is common according to Tzai, but is not in Cedict dictionary
+    // used only in the word: 薃葔
 
   }
 
 
+  it should "test get a string with Junda codes up to 6000" in {
+
+    val elementCodes: String =
+      sorted_sortCharactersSimplified
+        .filter(y => y._2.length == 4)
+        .groupBy(_._2)
+        .values
+        .filter(x => x.length > 9)
+        .map(x => x.drop(9))
+        .map(x => x.filter(y => AsingletonsForTests.junda.contains(y._1)))
+        .map(x => x.map(y => (y._1, y._2, AsingletonsForTests.junda.get(y._1).get)))
+        .map(x => x.filter(y => y._3 <= 6000))
+        .filter( x => x.size > 0)
+        .toList.sortBy(_.head._3)
+        .map(x => (x.head._2, x))
+        .map(x => (x._1, x._2.map(y => (y._1 + y._3).mkString("")).mkString(" ")))
+        .map(x => x._1 + " " + x._2).mkString("")
+
+    elementCodes shouldBe
+      "jjuo 藥3734" +
+        "njuo 藥3734" +
+        "ejoo 類4915" +
+        "jhmo 長4933" +
+        "nhmo 長4933" +
+        "jnho 聩5209 綦5599" +
+        "xhjo 嗉5351 題5105" +
+        "kxho 砹5772 硖5856" +
+        "yhso 锿5941" +
+        "pxjg 鮈5959" +
+        "jnxn 難5970"
+
+
+  }
+  
+  it should "test get a string with Tzai codes up to 6000" in {
+
+    val elementCodes: String =
+      sorted_sortCharactersTraditional
+        .filter(y => y._2.length == 4)
+        .groupBy(_._2)
+        .values
+        .filter(x => x.length > 9)
+        .map(x => x.drop(9))
+        .map(x => x.filter(y => AsingletonsForTests.tzai.contains(y._1)))
+        .map(x => x.map(y => (y._1, y._2, AsingletonsForTests.tzai.get(y._1).get)))
+        .map(x => x.filter(y => y._3 <= 6000))
+        .filter( x => x.size > 0)
+        .toList.sortBy(_.head._3)
+        .map(x => (x.head._2, x))
+        .map(x => (x._1, x._2.map(y => (y._1 + y._3).mkString("")).mkString(" ")))
+        .map(x => x._1 + " " + x._2).mkString("")
+
+    elementCodes shouldBe
+      "jjuo 葔3572" +
+        "njuo 葔3572" +
+        "yveo 秝5100" +
+        "hnjo 瑮5135" +
+        "jnxo 鞅5412 顴5454 茦5879" +
+        "kxho 饜5477 戛5769" +
+        "pxjh 魽5613" +
+        "njjh 荺5624" +
+        "jjjh 荺5624" +
+        "jjjn 蓳5631" +
+        "njjn 蓳5631" +
+        "pxjn 鰡5650" +
+        "jjgo 芵5661" +
+        "xjgo 嘳5724" +
+        "xhjo 暪5811" +
+        "wjgo 漯5821"
+
+
+  }
 
   }
